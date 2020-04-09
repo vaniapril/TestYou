848ba495c829711a25b0d01ba4ed001c427623d3
vaniapril <vaniapril@mail.ru>
2020-04-09 10:57:44 +0300

    create repository

--- Program.cs
+++ Program.cs
@@ -18,9 +18,6 @@ namespace TestYou
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
