507074915a8d228186657d9c2deff2b12d52fc7e
Maryan <marian200110@gmail.com>
2020-05-12 15:53:53 +0300

    Some fix in registration

---
+++ wwwroot/js/FuncLogin.js
@@ -0,0 +1,5 @@
+function AcceptData() {
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+//Далее надо просто напросто запизнуть это на сервер и найти есть ли такой user изи!!!
+}
