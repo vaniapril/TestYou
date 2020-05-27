684a9e67d92ee01fce41d2cadab1be404826fbb1
vaniapril <vaniapril@mail.ru>
2020-05-27 16:37:52 +0300

    default models

---
+++ Services/Models/DefaultModels.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace TestYou.Services.Models
+{
+    public static class DefaultModels
+    {
+        private static TestService _testService;
+        private static UserService _userService;
+        static DefaultModels()
+        {
+            _testService = new TestService();
+            _userService = new UserService();
+        }
+        
+        public static void Create()
+        {
+            CreateUsers();
+            CreateTests();
+        }
+        static void CreateUsers()
+        {
+            
+        }
+        static void CreateTests()
+        {
+            
+        }
+    }
+}
